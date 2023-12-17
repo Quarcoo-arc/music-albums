193b1dd7ca507ecf028977c73df0bd42151b1097
Michael Quarcoo <michaelquarcoo04@gmail.com>
2023-12-17 16:30:39 +0000

    Add http request methods

---
+++ src/helpers/httpRequests.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const getRequest = async (url: string) => {
+  try {
+    const response = await axios.get(url);
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    throw Error((error as string) || "Failed to fetch data");
+  }
+};
+
+export const deleteRequest = async (url: string) => {
+  try {
+    const response = await axios.delete(url);
+    console.log(response, response.data);
+
+    return response;
+  } catch (error) {
+    throw Error((error as string) || "Failed to delete resource");
+  }
+};
