4141cc16db61af38f5561494d780721f7f0f09d0
Michael Quarcoo <michaelquarcoo04@gmail.com>
2023-12-19 14:51:06 +0000

    Add theme and font config

--- App.tsx
+++ App.tsx
@@ -1,34 +1,75 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "screens/Home";
 import { Provider } from "react-redux";
 import store from "store";
+import {
+  PaperProvider,
+  MD3LightTheme as DefaultTheme,
+} from "react-native-paper";
 import { RootSiblingParent } from "react-native-root-siblings";
+import fonts from "config/fonts";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  const loadAssets = async () => {
+    await Font.loadAsync({
+      "Roboto-Bold": require("./src/assets/fonts/Roboto_Bold.ttf"),
+      "Roboto-Medium": require("./src/assets/fonts/Roboto_Medium.ttf"),
+      "Roboto-Regular": require("./src/assets/fonts/Roboto_Regular.ttf"),
+    });
+  };
+
   const Stack = createNativeStackNavigator();
+
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+    },
+  };
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await loadAssets();
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    prepare();
+  }, []);
+
+  useCallback(async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <Provider store={store}>
-      <RootSiblingParent>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-          </Stack.Navigator>
-          <StatusBar style="auto" />
-        </NavigationContainer>
-      </RootSiblingParent>
-    </Provider>
+    <PaperProvider theme={{ ...theme, fonts }}>
+      <Provider store={store}>
+        <RootSiblingParent>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+            <StatusBar style="auto" />
+          </NavigationContainer>
+        </RootSiblingParent>
+      </Provider>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    fontFamily: "Roboto_Medium",
-  },
-});
---
+++ src/config/fonts.ts
@@ -0,0 +1,27 @@
+import { configureFonts } from "react-native-paper";
+
+const baseFont = {
+  fontFamily: "Roboto-Regular",
+} as const;
+
+const baseVariants = configureFonts({ config: baseFont });
+
+const customVariants = {
+  displayMedium: {
+    ...baseVariants.displayMedium,
+    fontFamily: "Roboto-Bold",
+  },
+  bold: {
+    ...baseVariants.bodyMedium,
+    fontFamily: "Roboto-Bold",
+  },
+} as const;
+
+const fonts = configureFonts({
+  config: {
+    ...baseVariants,
+    ...customVariants,
+  },
+});
+
+export default fonts;
