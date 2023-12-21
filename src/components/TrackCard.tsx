6f5aa57f44a3b3339ae2fa851c05b7fc240ac275
Michael Quarcoo <michaelquarcoo04@gmail.com>
2023-12-21 06:08:04 +0000

    Add music-note icon to track card

--- src/components/TrackCard.tsx
+++ src/components/TrackCard.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Card, Text } from "react-native-paper";
 import { AlbumPhotoType } from "types";
+import { MaterialCommunityIcons as MaterialIcons } from "@expo/vector-icons";
 
 const TrackCard = ({ track }: { track: AlbumPhotoType }) => {
   return (
     <Card className="my-3" mode="elevated">
       <Card.Cover source={{ uri: track.thumbnailUrl }} />
-      <Card.Title title={<Text className="italic">{`# ${track.id}`}</Text>} />
+      <Card.Title
+        title={
+          <Text className="italic">
+            <MaterialIcons name="music-note" size={16} />
+            {track.id}
+          </Text>
+        }
+      />
       <Card.Content>
         <Text className="font-medium" variant="titleLarge">
           {track.title}
